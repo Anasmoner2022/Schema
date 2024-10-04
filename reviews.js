6e3145d4c11729d93a8df01e50b636351fcb4e88
Anasmoner2022 <99214491+Anasmoner2022@users.noreply.github.com>
2024-10-04 15:05:51 +0300

    Create reviews.js

---
+++ reviews.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var reviews = new Schema({
+  productId: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  customerId: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  comment: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    required: true
+  }
+});
